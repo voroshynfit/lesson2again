6ca5286bcbd0a66527c9847e314906879e83749d
sebastian <sebastian@sebastians-Mini.wikilink.by>
2024-08-13 23:15:22 +0300

    new project

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '11.0'
+use_frameworks!
+
+target 'lesson2' do
+   pod 'SwiftLint'
+end
---
+++ lesson2/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  lesson2
+//
+//  Created by sebastian on 13.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("стирай этот метод и пиши код сюда")
+    }
+}
+
